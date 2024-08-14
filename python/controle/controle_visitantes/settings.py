a40e68b3cbd23909ab75b728245e7c6ada218a3a
Fabiano <professor@fabianomoreira.com.br>
2024-08-14 17:50:13 -0300

    Criação da página de templates

--- python/controle/controle_visitantes/settings.py
+++ python/controle/controle_visitantes/settings.py
@@ -47,7 +47,7 @@ ROOT_URLCONF = 'controle_visitantes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -112,3 +112,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
